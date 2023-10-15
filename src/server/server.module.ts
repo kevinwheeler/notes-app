4b827ddab695ce2ff6c02bc47ec9ff4758e4799f
Kevin Wheeler <spreadlove5683@gmail.com>
2023-10-15 12:08:44 -0500

    Added rate limiting.

--- src/server/server.module.ts
+++ src/server/server.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 import { AppModule } from 'src/server/app/app.module';
 import { ViewModule } from 'src/server/view/view.module';
 
 @Module({
-  imports: [ViewModule, AppModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 30,
+      },
+    ]),
+    ViewModule,
+    AppModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class ServerModule {}
