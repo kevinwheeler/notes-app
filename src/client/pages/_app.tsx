3c3690a33cece177311e9804b3313e4e1e0edbda
Kevin Wheeler <spreadlove5683@gmail.com>
2023-10-18 10:55:00 -0500

    Added Tailwind.

--- src/client/pages/_app.tsx
+++ src/client/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import Script from 'next/script';
+import '../styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
