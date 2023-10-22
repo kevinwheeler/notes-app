3adb564e543f338fb503c02be688005d562d7365
Kevin Wheeler <spreadlove5683@gmail.com>
2023-10-22 15:14:17 -0500

    Added public directory for static files.

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import * as express from 'express';
 
 import { ServerModule } from 'src/server/server.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ServerModule);
   app.use(cookieParser());
-
+  app.use('/public', express.static(__dirname + '/../../public'));
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
