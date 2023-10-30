828a1815b8a8ea5e5f3f7c9a154c5d958efcdc05
Kevin Wheeler <spreadlove5683@gmail.com>
2023-10-30 12:53:26 -0500

    Removed tailwind CDN.

--- src/client/pages/_document.tsx
+++ src/client/pages/_document.tsx
@@ -21,7 +21,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
             rel="stylesheet"
           ></link>
-          <script src="https://cdn.tailwindcss.com"></script>
           <NextScript />
         </Head>
         <body>
